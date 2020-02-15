917372feb059bfebcb95dc77aa693624c06b720f
Abhijeet Soni <40241646+abhijeet-nkt@users.noreply.github.com>
2020-02-15 16:46:08 +0530

    Add a command to declare moderator roles (#4)

--- tests.py
+++ tests.py
@@ -12,12 +12,14 @@ respective classes.
 import unittest
 import repository as repo
 import sqlhelper as sql
+import repository as repo
 import os
 
 #Remove the comment to delete the `database.sql` file automatically.
 #This can be helpful for easing out the process.
 #DO NOT commit to git with the comment removed.
 # os.remove('database.sql')
+sql.create_tables()
 
 class TestPrefixCommand(unittest.TestCase):
     """
@@ -31,12 +33,60 @@ class TestPrefixCommand(unittest.TestCase):
 
         GUILD_ID = 55
 
-        sql.init_prefix_table()
-
         self.assertEquals(repo.get_prefix(GUILD_ID), '~')
 
         sql.update_guild_prefix(55, "%")
         self.assertEquals(repo.get_prefix(GUILD_ID), '%')
+
+class TestModRoleCommand(unittest.TestCase):
+    """
+    Test cases for `modrole` command
+    """
+
+    GUILD1 = 55
+    GUILD2 = 66
+    ROLE1 = 5
+    ROLE2 = 3
+
+    def test_add_remove(self):
+
+        self.assertTrue(
+            repo.add_mod_role(self.GUILD1, self.ROLE1)
+        )
+
+        self.assertTrue(
+            repo.add_mod_role(self.GUILD2, self.ROLE1)
+        )
+
+        self.assertTrue(
+            repo.delete_mod_role(self.GUILD1, self.ROLE1)
+        )
+
+        self.assertFalse(
+            repo.delete_mod_role(self.GUILD1, self.ROLE1)
+        )
+
+        self.assertFalse(
+            repo.delete_mod_role(self.GUILD1, self.ROLE2)
+        )
+
+        self.assertFalse(
+            repo.delete_mod_role(self.GUILD2, self.ROLE2)
+        )
+
+        self.assertTrue(
+            repo.delete_mod_role(self.GUILD2, self.ROLE1)
+        )
+
+
+        #These two stay in db
+        self.assertTrue(
+            repo.add_mod_role(self.GUILD1, self.ROLE2)
+        )
+
+        self.assertTrue(
+            repo.add_mod_role(self.GUILD2, self.ROLE2)
+        )
 
 if __name__ == '__main__':
     unittest.main()
