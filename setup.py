4a22efb423e180f0a450d30199289d8b6a8342bb
Abhijeet Soni <abhijeet.nkt@gmail.com>
2020-02-16 00:53:32 +0530

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="EquiBot",
+    version="0.0.1-dev",
+    author="Abhijeet Soni",
+    author_email="abhijeet.nkt@gmail.com",
+    url="https://github.com/abijeet-nkt/EquiBot",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.7"
+)
