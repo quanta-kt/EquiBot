1a29215ad2124c81cf48d2e19ed3aeb9b6e38553
Abhijeet Soni <abhijeet.nkt@gmail.com>
2020-02-17 16:37:06 +0530

    Move isModeratorOrOwner to util.py

--- equibot/app.py
+++ equibot/app.py
@@ -6,6 +6,7 @@ import asyncio
 
 from . import constants
 from . import repository
+from . import util
 
 bot = commands.Bot(
     command_prefix = lambda bot, message:
@@ -131,6 +132,10 @@ async def modrole_remove(ctx: commands.Context, *args):
 
         return
 
+    if not await util.isModeratorOrOwner(ctx, repo):
+        await ctx.send("You're not allowed to issue this command. ;-;")
+        return
+
     role = discord.utils.find(
         lambda role: role.name == args[0] or role.mention == args[0],
         ctx.guild.roles
@@ -153,19 +158,6 @@ async def clear(ctx: commands.Context, *args):
     Deletes a specified number of messages from the channel.
     Deletes 10 messages if a number was not specified.
     """
-
-    async def isModeratorOrOwner():
-        moderators = await repo.get_all_mod_roles(ctx.guild.id)
-        for mod in moderators:
-            if mod in map(lambda role: role.id, ctx.author.roles):
-                return True
-        return False
-            
-        #or ctx.author == ctx.guild.owner
-
-    if not await isModeratorOrOwner():
-        await ctx.send("You're not allowed to issue this command. ;-;")
-        return
 
     n = 10
 
